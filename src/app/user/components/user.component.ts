f258fae305bdc704f926d939c77a5d3e8df05024
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2022-07-23 00:57:45 +0200

    user list

--- src/app/user/components/user.component.ts
+++ src/app/user/components/user.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject, Subscription} from "rxjs";
+import {UserModel} from "../model/userModel";
+import {UserService} from "../services/user.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+    selector: 'app-user',
+    templateUrl: './user.component.html',
+    styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+    public usersList: UserModel[] = [];
+    public subscriptions: Subscription[] = [];
+    public notificationMsg: string = '';
+    public showBanner: boolean = true;
+    public isFetchingData: boolean = false;
+    private titleSubject = new BehaviorSubject<string>('');
+    public titleAction$ = this.titleSubject.asObservable();
 
-  constructor() { }
+    constructor(private userService: UserService) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.getAllUsers(true);
+    }
 
+    public changeTitle(title: string): void {
+        this.titleSubject.next(title);
+    }
+
+    public getAllUsers(showNotification: boolean): void {
+        this.isFetchingData = true;
+        this.subscriptions.push(
+            this.userService.getUsers().subscribe((response: UserModel[]) => {
+                this.userService.addUsersToLocalStorage(response);
+                this.usersList = response;
+                this.isFetchingData = false;
+                if (showNotification) {
+                    this.notificationMsg = `${response.length} utilisateurs chargés`;
+                    this.showBanner = true;
+                    setTimeout(() => {
+                        this.showBanner = false
+                    }, 3000);
+                }
+            }, (error: HttpErrorResponse) => {
+                this.notificationMsg = error.error.responseDescription;
+                this.showBanner = false
+            })
+        );
+    }
 }
--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -7,62 +7,62 @@ import {CustomHttpResponse} from "../../http/model/customHttpResponse";
 
 @Injectable({providedIn: 'root'})
 export class UserService {
-  private apiUrl: string = environment.apiUrl;
+    private apiUrl: string = environment.apiUrl;
 
-  constructor(private http: HttpClient) {
-  }
-
-  public getUsers(): Observable<UserModel[] | HttpErrorResponse> {
-    return this.http.get<UserModel[]>(`${this.apiUrl}/user/list`)
-  }
-
-  public addUser(formData: FormData): Observable<UserModel | HttpErrorResponse> {
-    return this.http.post<UserModel>(`${this.apiUrl}/user/add`, formData);
-  }
-
-  public updateUser(formData: FormData): Observable<UserModel | HttpErrorResponse> {
-    return this.http.post<UserModel>(`${this.apiUrl}/user/update`, formData);
-  }
-
-  public resetPassword(userEmail: string): Observable<CustomHttpResponse | HttpErrorResponse> {
-    return this.http.get<CustomHttpResponse>(`${this.apiUrl}/user/resetPassword/${userEmail}`);
-  }
-
-  public updateUserProfileImg(formData: FormData): Observable<HttpEvent<UserModel> | HttpErrorResponse> {
-    return this.http.post<UserModel>(`${this.apiUrl}/user/updateProfileImage`, formData, {
-      //track upload progress to display it
-      reportProgress: true,
-      observe: 'events'
-    });
-  }
-
-  public deleteUser(userId: number): Observable<CustomHttpResponse | HttpErrorResponse> {
-    return this.http.delete<CustomHttpResponse>(`${this.apiUrl}/user/delete/${userId}`);
-  }
-
-  public addUsersToLocalStorage(users: UserModel): void {
-    localStorage.setItem('users', JSON.stringify(users));
-  }
-
-  public getUsersFromLocalStorage(): UserModel[] | null {
-    if (localStorage.getItem('users')) {
-      return JSON.parse(localStorage.getItem('users')!);
+    constructor(private http: HttpClient) {
     }
-    return null;
-  }
 
-  public createUserFormData(connectedUserAccountName: string, user: UserModel, userProfileImage: File): FormData {
-    const formData = new FormData();
-    formData.append('currentUserFirstName', 'connectedUserAccountName');
-    formData.append('userFirstName', user.userFirstName);
-    formData.append('userLastName', user.userLastName);
-    formData.append('userAccountName', user.userAccountName);
-    formData.append('userEmail', user.userEmail);
-    formData.append('userRole', user.userRole);
-    formData.append('userProfilePictureLink', userProfileImage);
-    formData.append('isUserConnected', JSON.stringify(user.isUserConnected));
-    formData.append('isUserNotBanned', JSON.stringify(user.isUserNotBanned));
-    return formData;
-  }
+    public getUsers(): Observable<UserModel[]> {
+        return this.http.get<UserModel[]>(`${this.apiUrl}/user/list`)
+    }
+
+    public addUser(formData: FormData): Observable<UserModel> {
+        return this.http.post<UserModel>(`${this.apiUrl}/user/add`, formData);
+    }
+
+    public updateUser(formData: FormData): Observable<UserModel> {
+        return this.http.post<UserModel>(`${this.apiUrl}/user/update`, formData);
+    }
+
+    public resetPassword(userEmail: string): Observable<CustomHttpResponse | HttpErrorResponse> {
+        return this.http.get<CustomHttpResponse>(`${this.apiUrl}/user/resetPassword/${userEmail}`);
+    }
+
+    public updateUserProfileImg(formData: FormData): Observable<HttpEvent<UserModel> | HttpErrorResponse> {
+        return this.http.post<UserModel>(`${this.apiUrl}/user/updateProfileImage`, formData, {
+            //track upload progress to display it
+            reportProgress: true,
+            observe: 'events'
+        });
+    }
+
+    public deleteUser(userId: number): Observable<CustomHttpResponse> {
+        return this.http.delete<CustomHttpResponse>(`${this.apiUrl}/user/delete/${userId}`);
+    }
+
+    public addUsersToLocalStorage(users: UserModel[]): void {
+        localStorage.setItem('users', JSON.stringify(users));
+    }
+
+    public getUsersFromLocalStorage(): UserModel[] | null {
+        if (localStorage.getItem('users')) {
+            return JSON.parse(localStorage.getItem('users')!);
+        }
+        return null;
+    }
+
+    public createUserFormData(connectedUserAccountName: string, user: UserModel, userProfileImage: File): FormData {
+        const formData = new FormData();
+        formData.append('currentUserFirstName', 'connectedUserAccountName');
+        formData.append('userFirstName', user.userFirstName);
+        formData.append('userLastName', user.userLastName);
+        formData.append('userAccountName', user.userAccountName);
+        formData.append('userEmail', user.userEmail);
+        formData.append('userRole', user.userRole);
+        formData.append('userProfilePictureLink', userProfileImage);
+        formData.append('isUserConnected', JSON.stringify(user.isUserConnected));
+        formData.append('isUserNotBanned', JSON.stringify(user.isUserNotBanned));
+        return formData;
+    }
 
 }
